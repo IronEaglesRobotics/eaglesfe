dcb43ffe2f216ec2734535b8ec78840c07d27426
masme <garciama@student.saviochs.org>
2018-11-03 11:48:44 -0500

    pt2

--- TeamCode/src/main/java/eaglesfe/roverruckus/opmodes/autonomous/CompetitionAutonomous.java
+++ TeamCode/src/main/java/eaglesfe/roverruckus/opmodes/autonomous/CompetitionAutonomous.java
@@ -1,8 +1,11 @@
 package eaglesfe.roverruckus.opmodes.autonomous;
+import android.graphics.Point;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import eaglesfe.common.BirdseyeServer;
 import eaglesfe.common.FieldPosition;
+import eaglesfe.common.MathHelpers;
 import eaglesfe.roverruckus.IronEaglesRobotRoverRuckus;
 import eaglesfe.roverruckus.opmodes.RoverRuckusBirdseyeAutonomous;
 
@@ -20,18 +23,37 @@ public class CompetitionAutonomous extends RoverRuckusBirdseyeAutonomous {
         birdseye = BirdseyeServer.GetInstance(3708, this.telemetry);
         birdseye.start();
 
+        int currentTarget = 0;
+        int[][] target = {
+                {0,0},
+                {10,10}
+        };
+
         robot = new IronEaglesRobotRoverRuckus(hardwareMap);
 
         FieldPosition currentPosition;
         waitForStart();
+
+        moveTowardHeightTime(.5, 500);
+
         while(opModeIsActive()){
+            final Point TARGET = new Point(target[currentTarget][0], target[currentTarget][1]);
             currentPosition = getPosition();
-            birdseye.broadcast(currentPosition.asJSONObject());
+
+            if (currentTarget <= target.length) {
+
+                birdseye.broadcast(currentPosition.asJSONObject());
+
+                double rise = TARGET.y - currentPosition.y;
+                double run = TARGET.x - currentPosition.x;
+
+                moveTowardPosition(run, rise);
+            }
 
             this.addPositionToTelemetry();
             telemetry.update();
 
-            this.sleep(100);
+            if ((currentPosition.x - target[currentTarget][0] < 10) && (currentPosition.y - target[currentTarget][1] < 10)) {currentTarget++;}
         }
 
         try {
@@ -44,4 +66,11 @@ public class CompetitionAutonomous extends RoverRuckusBirdseyeAutonomous {
     public void moveTowardPosition(double x, double y){
         robot.get_drive().updateMotors(x, y, 0);
     }
+
+    //public void moveTowardHeight(float z) { robot.get_Arms().updateArms(z,-z,0, 0); }
+
+    public void moveTowardHeightTime(double z, int millis) { robot.get_Arms().updateArmsTime(z, 0,0,0, millis);}
+
 }
+
+
